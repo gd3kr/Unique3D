7d7bb6e32422c9d6f6a345779c87ddc21aa7f600
Aarya <63528145+gd3kr@users.noreply.github.com>
2024-09-07 14:21:40 -0500

    uncomment compilation for now

--- scripts/sd_model_zoo.py
+++ scripts/sd_model_zoo.py
@@ -138,27 +138,27 @@ def load_common_sd15_pipe(base_model=DEFAULT_BASE_MODEL, device="auto", controln
         pipe.enable_vae_slicing()
         
 
-    config = CompilationConfig.Default()
-    # xformers and Triton are suggested for achieving best performance.
-    try:
-        import xformers
-        config.enable_xformers = True
-    except ImportError:
-        print('xformers not installed, skip')
-    try:
-        import triton
-        config.enable_triton = True
-    except ImportError:
-        print('Triton not installed, skip')
-    # CUDA Graph is suggested for small batch sizes and small resolutions to reduce CPU overhead.
-    # But it can increase the amount of GPU memory used.
-    # For StableVideoDiffusionPipeline it is not needed.
+    # config = CompilationConfig.Default()
+    # # xformers and Triton are suggested for achieving best performance.
+    # try:
+    #     import xformers
+    #     config.enable_xformers = True
+    # except ImportError:
+    #     print('xformers not installed, skip')
+    # try:
+    #     import triton
+    #     config.enable_triton = True
+    # except ImportError:
+    #     print('Triton not installed, skip')
+    # # CUDA Graph is suggested for small batch sizes and small resolutions to reduce CPU overhead.
+    # # But it can increase the amount of GPU memory used.
+    # # For StableVideoDiffusionPipeline it is not needed.
 
-    config.enable_cuda_graph = True
+    # config.enable_cuda_graph = True
 
-    print("compiling model")
-    pipe = compile(pipe, config)
-    print("model compiled")
+    # print("compiling model")
+    # pipe = compile(pipe, config)
+    # print("model compiled")
 
 
     import gc
