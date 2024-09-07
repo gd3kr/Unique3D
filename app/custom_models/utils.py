a666d11636959fdd4a77b013021dca867c58b02f
Aarya <63528145+gd3kr@users.noreply.github.com>
2024-09-06 19:50:48 -0500

    dont compile sr model

--- app/custom_models/utils.py
+++ app/custom_models/utils.py
@@ -77,25 +77,25 @@ def load_pipeline(config_path, ckpt_path, pipeline_filter=lambda x: True, weight
     pipeline = pipeline.to(device, dtype=weight_dtype)
 
 
-    config = CompilationConfig.Default()
-    # xformers and Triton are suggested for achieving best performance.
-    try:
-        import xformers
-        config.enable_xformers = True
-    except ImportError:
-        print('xformers not installed, skip')
-    try:
-        import triton
-        config.enable_triton = True
-    except ImportError:
-        print('Triton not installed, skip')
-    # CUDA Graph is suggested for small batch sizes and small resolutions to reduce CPU overhead.
-    # But it can increase the amount of GPU memory used.
-    # For StableVideoDiffusionPipeline it is not needed.
-    config.enable_cuda_graph = True
+    # config = CompilationConfig.Default()
+    # # xformers and Triton are suggested for achieving best performance.
+    # try:
+    #     import xformers
+    #     config.enable_xformers = True
+    # except ImportError:
+    #     print('xformers not installed, skip')
+    # try:
+    #     import triton
+    #     config.enable_triton = True
+    # except ImportError:
+    #     print('Triton not installed, skip')
+    # # CUDA Graph is suggested for small batch sizes and small resolutions to reduce CPU overhead.
+    # # But it can increase the amount of GPU memory used.
+    # # For StableVideoDiffusionPipeline it is not needed.
+    # config.enable_cuda_graph = True
 
-    print("compiling model")
-    pipeline = compile(pipeline, config)
-    print("model compiled")
+    # print("compiling model")
+    # pipeline = compile(pipeline, config)
+    # print("model compiled")
 
     return trainer_out, pipeline
